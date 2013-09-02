519f5ddfdf78b4a7f4b3d0ffc697ecd3a1ef37a6
Matthew McCants <mattmccants@gmail.com>
2013-09-02 11:54:38 -0500

    Started models work.

--- boulange/models.py
+++ boulange/models.py
@@ -1,3 +1,22 @@
-"""Just an empty models file to let the testrunner recognize this as app."""
-# from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class InventoryItem(models.Model):
+	name = models.CharField(max_length=70)
+	inner_pack = models.BooleanField(default=False)
+	inner_pack_size = models.IntegerField()
+	case_size = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+@python_2_unicode_compatible
+class OnHandInventoryItem(models.Model):
+	item = models.ForeignKey(InventoryItem)
+	eaches = models.IntegerField()
+	inner_packs = models.IntegerField()
+	cases = models.IntegerField()
+
+	def __str__(self):
+		return self.item.name
